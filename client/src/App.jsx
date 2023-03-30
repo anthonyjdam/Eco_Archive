5c5fd6498e79a109dba3d9bb5a1acb9372298911
jRaimuu <liamsarj4@gmail.com>
2023-03-29 22:15:56 -0600

    Changed button placement

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,9 @@ function App() {
 
   return (
     <>
-      {/* <Login /> */}
+      <Login />
       {/* <AdminSidebar /> */}
-      <SignUpCustomer />
+      {/* <SignUpCustomer /> */}
     </>
   )
 }
