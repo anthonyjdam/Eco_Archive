2d925f205630e7374be871e5155db2c06fad0be7
anthonyjdam <anthonydam1968@gmail.com>
2023-04-03 18:40:01 -0600

    Adjusted customer table

--- eco_archive.sql
+++ eco_archive.sql
@@ -1,4 +1,7 @@
-CREATE TABLE `eco_archive`.`customer` (
+-- Create the customer table
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
   `UserID` INT NOT NULL AUTO_INCREMENT,
   `Username` VARCHAR(255) NOT NULL,
   `FName` VARCHAR(255) NOT NULL,
@@ -6,11 +9,14 @@ CREATE TABLE `eco_archive`.`customer` (
   `Password` VARCHAR(255) NOT NULL,
   `AccountBal` DECIMAL(15,2) NOT NULL DEFAULT 0.00,
   `DonationAmt` DECIMAL(15,2) NOT NULL DEFAULT 0.00,
+  `AddressLine` VARCHAR(255) NULL,
   `City` VARCHAR(255) NOT NULL,
   `Province` VARCHAR(2) NOT NULL,
-  `AddressNumber` VARCHAR(255) NOT NULL,
-  `Street` VARCHAR(255) NOT NULL,
-  `ApartmentNumber` VARCHAR(255) NULL,
-  PRIMARY KEY (`UserID`),
-  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE,
-  UNIQUE INDEX `UserID_UNIQUE` (`UserID` ASC) VISIBLE);
+  `PostalCode` VARCHAR(255) NULL,
+  PRIMARY KEY (`UserID`)
+  );
+  
+  -- Dump data into customer table
+  LOCK TABLES `customer` WRITE;
+  INSERT INTO `customer` VALUES (1, "anthonyjdam", "Anthony", "Dam", "hello", 1000.00, 0.00, "123 Anthony St NW", "Calgary", "AB", "T1T1T1"), (2, "jRaimuu", "Liam", "Sarjeant", "eco", 6969.00, 0.00, "123 Liam St NW", "Calgary", "AB", "A1B2C3"), (3, "mairakhan419", "Maira", "Khan", "archive", 696969.00, 0.00, "123 Maira St NW", "Calgary", "AB", "D4E5F6");
+  UNLOCK TABLES;
